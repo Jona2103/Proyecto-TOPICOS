14ae0d84b208e4f6bfa5364c9b62184a700a96d9
52311 <52311@OswaldoDiaz>
2024-11-23 14:35:56 -0600

    PRUEBA 2

---
+++ src/activatec/GG.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package activatec;
+
+/**
+ *
+ * @author 52311
+ */
+public class GG {
+    
+}
